d1f5b0d1392488b8ac481b3628abefbafcc023c9
unknown <yakup.senel33@gmail.com>
2021-12-30 15:47:11 +0100

    👌 IMPROVE:

--- exercice1.php
+++ exercice1.php
@@ -1,12 +1,13 @@
 <?php
 
 //Méthode 1
-    $mot = "Salut je m'appelle Yakup"."\n";
+    $mot = "Salut je m'appelle Yakup";
     $result = "";
     for($i=strlen($mot)-1;$i>0;$i--){
         $result = $mot[$i];
         echo $result;
     }
+    echo "\n";
 
 //Méthode 2
     echo strrev($mot);
